874cd4dc7f65c9749b8b27171635ac509b584f37
Ali <aliyetim1207@gmail.com>
2024-09-26 14:20:12 +0300

    Resume-Restart added.
    Esc ile ana menü açılabilir. Resume, Restart gibi fonksiyonlar kullanılabilir.

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -13,9 +13,8 @@ public class GUIScript : MonoBehaviour
 	
 	public TextMeshProUGUI scoreText;
 	public TextMeshProUGUI highestScoreText;
-	
-	
-	
+	public GameObject pauseMenu;
+	private bool isPaused = false;
 	
 
 	// Start is called before the first frame update
@@ -23,20 +22,60 @@ public class GUIScript : MonoBehaviour
 	{
 		GameObject player = GameObject.FindWithTag("Player");
 		ps = player.GetComponent<PlayerScript>();
-		//scoreText.text = "Score: ";
-	}
-
-	// Update is called once per frame
-	void FixedUpdate()
-	{
-		scoreText.text = "Score: "+ps.score.ToString();
-		highestScoreText.text = "Best Score: " + ps.highestScore.ToString();
 		
 	}
 
-	public void RetryGame()
-    {
-        // Reload the current active scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		scoreText.text = "Score: "+ps.score.ToString();
+		highestScoreText.text = "Best Score: " + ps.highestScore.ToString();
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (isPaused)
+			{
+				pauseMenu.SetActive(false);
+				Resume(); 
+				
+				// Resume if currently paused
+			}
+			else
+			{
+				OpenMainMenu(); // Pause if not already paused
+			}
+		}
+		
+	
+	}
+
+	public void OpenMainMenu()
+	{
+		Pause();
+		pauseMenu.SetActive(true);
+	}
+	public void ResumeButton()
+	{
+		pauseMenu.SetActive(false);
+		Resume();
+	}
+	public void RestartButton()
+	{
+		SceneManager.LoadScene("Gameplay");
+		Resume();
+	}
+	public void OptionsButton()
+	{
+		
+	}
+	
+	public void Pause()
+	{
+		isPaused = true;
+		Time.timeScale = 0f;
+	}
+	public void Resume()
+	{
+		Time.timeScale = 1f;
+		isPaused = false;
+	}
 }
